07001448cf0d5460278173fd693441fe87f3f1b4
caheo68a <caheo68a@icloud.com>
2026-03-29 16:02:50 +0700

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T09:02:30.995Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-29T09:02:50.125Z
+Commit: Fix broken links in documentation
