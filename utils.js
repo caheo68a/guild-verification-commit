0f5ca0d191ae89fe97d84b98a33691d849718ec0
caheo68a <caheo68a@icloud.com>
2026-03-29 16:03:30 +0700

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T09:03:06.625Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-29T09:03:29.601Z
+Commit: Fix CSS styling bug on mobile devices
