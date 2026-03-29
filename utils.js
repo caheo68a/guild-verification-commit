417d81283360b32691713edff308b839239fcb02
caheo68a <caheo68a@icloud.com>
2026-03-29 16:03:05 +0700

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T09:02:15.235Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-29T09:03:04.955Z
+Commit: Correct typo in configuration file
